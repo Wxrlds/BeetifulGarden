2d21ee31f4baa1e16742a6ba961401eb268dda75
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2025-06-22 17:12:42 +0200

    Don't reinvent the wheel for tooltips

--- src/main/java/eu/wxrlds/beetifulgarden/util/Effects.java
+++ src/main/java/eu/wxrlds/beetifulgarden/util/Effects.java
@@ -4,21 +4,26 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Effects {
     public static MobEffectInstance[] ConfigEffectsToEffectInstanceList(String effectString) {
-        if (!effectString.isEmpty()) {
-            String[] effectStrings = effectString.split("\\|");
-            MobEffectInstance[] effectInstances = new MobEffectInstance[effectStrings.length];
-            for (int i = 0; i < effectStrings.length; i++) {
-                String[] parts = effectStrings[i].split(":");
-                String modID = parts[0];
-                String effectID = parts[1];
-                int duration = Integer.parseInt(parts[2]);
-                int amplifier = Integer.parseInt(parts[3]);
-                effectInstances[i] = new MobEffectInstance(ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(modID, effectID)), duration, amplifier);
-            }
-            return effectInstances;
+        if (effectString.isEmpty()) {
+            return new MobEffectInstance[0];
         }
-        return new MobEffectInstance[0];
+        String[] effectStrings = effectString.split("\\|");
+        List<MobEffectInstance> effectList = new ArrayList<>();
+        for (String effect : effectStrings) {
+            String[] parts = effect.split(":");
+
+            String modID = parts[0];
+            String effectID = parts[1];
+            int duration = Integer.parseInt(parts[2]);
+            int amplifier = Integer.parseInt(parts[3]);
+
+            effectList.add(new MobEffectInstance(ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(modID, effectID)), duration, amplifier));
+        }
+        return effectList.toArray(new MobEffectInstance[0]);
     }
 }
