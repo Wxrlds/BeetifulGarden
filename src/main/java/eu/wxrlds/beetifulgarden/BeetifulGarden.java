f872e1a53f790e1c23665c173e80ec7fc83b8ca7
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-15 21:02:39 +0100

    Fix game crashing when there is a Wandering Trader at nighttime

--- src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
+++ src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
@@ -4,13 +4,10 @@ import eu.wxrlds.beetifulgarden.block.ModBlocks;
 import eu.wxrlds.beetifulgarden.config.BeetifulGardenCommonConfigs;
 import eu.wxrlds.beetifulgarden.item.ModItems;
 import eu.wxrlds.beetifulgarden.util.AppleSkinEventHandler;
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -19,16 +16,12 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BeetifulGarden.MOD_ID)
--- src/main/java/eu/wxrlds/beetifulgarden/util/ModEvents.java
+++ src/main/java/eu/wxrlds/beetifulgarden/util/ModEvents.java
@@ -16,68 +16,70 @@ public class ModEvents {
     @SubscribeEvent
     public static void applyBeetifulEffectOnEaten(LivingEntityUseItemEvent.Finish event) {
         // Only execute on server
-        if (!event.getEntity().level.isClientSide()) {
-
-            PlayerEntity player = (PlayerEntity) event.getEntityLiving();
-
-            EffectInstance[] effects;
-            FoodStats foodStats = player.getFoodData();
-            int nutritionValue;
-            double saturationValue;
-
-            // Get potion effects and food values
-            Item item = event.getItem().getItem();
-            if (item == ModItems.CLOUDY_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.CLOUDY_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.CLOUDY_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.CLOUDY_SATURATION.get();
-            } else if (item == ModItems.EMINENCE_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.EMINENCE_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.EMINENCE_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.EMINENCE_SATURATION.get();
-            } else if (item == ModItems.MARINE_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.MARINE_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.MARINE_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.MARINE_SATURATION.get();
-            } else if (item == ModItems.OLIVE_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.OLIVE_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.OLIVE_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.OLIVE_SATURATION.get();
-            } else if (item == ModItems.PISTACHIO_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.PISTACHIO_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.PISTACHIO_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.PISTACHIO_SATURATION.get();
-            } else if (item == ModItems.PIXIE_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.PIXIE_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.PIXIE_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.PIXIE_SATURATION.get();
-            } else if (item == ModItems.SIENNA_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.SIENNA_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.SIENNA_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.SIENNA_SATURATION.get();
-            } else if (item == ModItems.VELVET_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VELVET_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.VELVET_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.VELVET_SATURATION.get();
-            } else if (item == ModItems.VERDANT_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VERDANT_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.VERDANT_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.VERDANT_SATURATION.get();
-            } else if (item == ModItems.VERDIGRIS_BEETIFUL.get()) {
-                effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VERDIGRIS_EFFECTS.get()).toArray(new EffectInstance[0]);
-                nutritionValue = BeetifulGardenCommonConfigs.VERDIGRIS_NUTRITION.get();
-                saturationValue = BeetifulGardenCommonConfigs.VERDIGRIS_SATURATION.get();
-            } else {
-                // If the item is not one of the Beetifuls, then do not execute the other code
-                return;
-            }
-
-            for (EffectInstance effect : effects) {
-                player.addEffect(effect);
-            }
-            // We need to cap it at 20 or the food value can go over the vanilla limit
-            foodStats.setSaturation(Math.min(20, foodStats.getSaturationLevel() + (float) saturationValue));
-            foodStats.setFoodLevel(Math.min(20, foodStats.getFoodLevel() + nutritionValue));
+        // Make sure this only triggers for players and not Wandering Traders
+        if (event.getEntity().level.isClientSide() || !(event.getEntityLiving() instanceof PlayerEntity)) {
+            return;
         }
+
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+
+        EffectInstance[] effects;
+        FoodStats foodStats = player.getFoodData();
+        int nutritionValue;
+        double saturationValue;
+
+        // Get potion effects and food values
+        Item item = event.getItem().getItem();
+        if (item == ModItems.CLOUDY_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.CLOUDY_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.CLOUDY_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.CLOUDY_SATURATION.get();
+        } else if (item == ModItems.EMINENCE_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.EMINENCE_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.EMINENCE_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.EMINENCE_SATURATION.get();
+        } else if (item == ModItems.MARINE_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.MARINE_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.MARINE_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.MARINE_SATURATION.get();
+        } else if (item == ModItems.OLIVE_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.OLIVE_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.OLIVE_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.OLIVE_SATURATION.get();
+        } else if (item == ModItems.PISTACHIO_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.PISTACHIO_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.PISTACHIO_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.PISTACHIO_SATURATION.get();
+        } else if (item == ModItems.PIXIE_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.PIXIE_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.PIXIE_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.PIXIE_SATURATION.get();
+        } else if (item == ModItems.SIENNA_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.SIENNA_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.SIENNA_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.SIENNA_SATURATION.get();
+        } else if (item == ModItems.VELVET_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VELVET_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.VELVET_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.VELVET_SATURATION.get();
+        } else if (item == ModItems.VERDANT_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VERDANT_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.VERDANT_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.VERDANT_SATURATION.get();
+        } else if (item == ModItems.VERDIGRIS_BEETIFUL.get()) {
+            effects = Effects.ConfigEffectsToEffectInstanceList(BeetifulGardenCommonConfigs.VERDIGRIS_EFFECTS.get()).toArray(new EffectInstance[0]);
+            nutritionValue = BeetifulGardenCommonConfigs.VERDIGRIS_NUTRITION.get();
+            saturationValue = BeetifulGardenCommonConfigs.VERDIGRIS_SATURATION.get();
+        } else {
+            // If the item is not one of the Beetifuls, then do not execute the other code
+            return;
+        }
+
+        for (EffectInstance effect : effects) {
+            player.addEffect(effect);
+        }
+        // We need to cap it at 20 or the food value can go over the vanilla limit
+        foodStats.setSaturation(Math.min(20, foodStats.getSaturationLevel() + (float) saturationValue));
+        foodStats.setFoodLevel(Math.min(20, foodStats.getFoodLevel() + nutritionValue));
     }
 }
