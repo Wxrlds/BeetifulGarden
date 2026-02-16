2deef7e7ccf03a4a29b0f31814fd4ab8d4defcd2
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-16 16:53:41 +0100

    Consolidate redundant fruit and crop classes into generic implementations

--- src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulFruitItem.java
+++ src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulFruitItem.java
@@ -1,6 +1,5 @@
-package eu.wxrlds.beetifulgarden.item.fruit;
+package eu.wxrlds.beetifulgarden.item;
 
-import eu.wxrlds.beetifulgarden.config.BeetifulGardenCommonConfigs;
 import eu.wxrlds.beetifulgarden.util.Effects;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -14,16 +13,35 @@ import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.function.Supplier;
 
-public class SiennaBeetiful extends Item {
-    public SiennaBeetiful(Properties properties) {
+public class BeetifulFruitItem extends Item {
+    private final Supplier<String> effectsConfig;
+    private final Supplier<Integer> nutritionConfig;
+    private final Supplier<Double> saturationConfig;
+
+    public BeetifulFruitItem(Properties properties, Supplier<String> effects, Supplier<Integer> nutrition, Supplier<Double> saturation) {
         super(properties.food(new FoodProperties.Builder().alwaysEat().build()));
+        this.effectsConfig = effects;
+        this.nutritionConfig = nutrition;
+        this.saturationConfig = saturation;
+    }
+
+    public String getEffectString() {
+        return effectsConfig.get();
+    }
+
+    public int getNutrition() {
+        return nutritionConfig.get();
+    }
+
+    public float getSaturation() {
+        return saturationConfig.get().floatValue();
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-        String effectString = BeetifulGardenCommonConfigs.SIENNA_EFFECTS.get();
-        List<MobEffectInstance> mobEffects = Effects.ConfigEffectsToEffectInstanceList(effectString);
+        List<MobEffectInstance> mobEffects = Effects.ConfigEffectsToEffectInstanceList(getEffectString());
 
         // Create a fake potion ItemStack to generate tooltip with custom effects.
         // The actual item can't store effects directly since we load the effect from the config file,
--- src/main/java/eu/wxrlds/beetifulgarden/item/ModItems.java
+++ src/main/java/eu/wxrlds/beetifulgarden/item/ModItems.java
@@ -2,6 +2,7 @@ package eu.wxrlds.beetifulgarden.item;
 
 import eu.wxrlds.beetifulgarden.BeetifulGarden;
 import eu.wxrlds.beetifulgarden.ModGroup;
+import eu.wxrlds.beetifulgarden.config.BeetifulGardenCommonConfigs;
 import eu.wxrlds.beetifulgarden.item.fruit.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
@@ -14,44 +15,85 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BeetifulGarden.MOD_ID);
 
     public static final RegistryObject<Item> CLOUDY_BEETIFUL = ITEMS.register("cloudy_beetiful",
-            () -> new CloudyBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.CLOUDY_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.CLOUDY_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.CLOUDY_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> EMINENCE_BEETIFUL = ITEMS.register("eminence_beetiful",
-            () -> new EminenceBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.EMINENCE_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.EMINENCE_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.EMINENCE_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> MARINE_BEETIFUL = ITEMS.register("marine_beetiful",
-            () -> new MarineBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.MARINE_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.MARINE_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.MARINE_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> OLIVE_BEETIFUL = ITEMS.register("olive_beetiful",
-            () -> new OliveBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.OLIVE_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.OLIVE_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.OLIVE_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> PISTACHIO_BEETIFUL = ITEMS.register("pistachio_beetiful",
-            () -> new PistachioBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.PISTACHIO_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.PISTACHIO_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.PISTACHIO_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> PIXIE_BEETIFUL = ITEMS.register("pixie_beetiful",
-            () -> new PixieBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.PIXIE_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.PIXIE_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.PIXIE_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> SIENNA_BEETIFUL = ITEMS.register("sienna_beetiful",
-            () -> new SiennaBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.SIENNA_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.SIENNA_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.SIENNA_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> VELVET_BEETIFUL = ITEMS.register("velvet_beetiful",
-            () -> new VelvetBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.VELVET_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.VELVET_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.VELVET_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> VERDANT_BEETIFUL = ITEMS.register("verdant_beetiful",
-            () -> new VerdantBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.VERDANT_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.VERDANT_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.VERDANT_SATURATION::get
+            ));
 
     public static final RegistryObject<Item> VERDIGRIS_BEETIFUL = ITEMS.register("verdigris_beetiful",
-            () -> new VerdigrisBeetiful(
-                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP)));
+            () -> new BeetifulFruitItem(
+                    new Item.Properties().tab(ModGroup.BEETIFULGARDEN_GROUP),
+                    BeetifulGardenCommonConfigs.VERDIGRIS_EFFECTS::get,
+                    BeetifulGardenCommonConfigs.VERDIGRIS_NUTRITION::get,
+                    BeetifulGardenCommonConfigs.VERDIGRIS_SATURATION::get
+            ));
+
 
     public static final RegistryObject<Item> BEETIFUL_SEEDS = ITEMS.register("beetiful_seeds",
             () -> new BeetifulSeed(
