66a81bcd2adbf8e9cfe55c90b86d169d6b1841f1
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-16 17:23:16 +0100

    Consolidate redundant fruit and crop classes into generic implementations

--- src/main/java/eu/wxrlds/beetifulgarden/block/BeetifulCropBlock.java
+++ src/main/java/eu/wxrlds/beetifulgarden/block/BeetifulCropBlock.java
@@ -1,6 +1,5 @@
-package eu.wxrlds.beetifulgarden.block.crop;
+package eu.wxrlds.beetifulgarden.block;
 
-import eu.wxrlds.beetifulgarden.config.BeetifulGardenCommonConfigs;
 import eu.wxrlds.beetifulgarden.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -10,20 +9,23 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.BeetrootBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.IPlantable;
 
-public class SiennaCrop extends BeetrootBlock {
-    private Block getPlantableOn() {
-        String configValue = BeetifulGardenCommonConfigs.SIENNA_PLANTABLE_ON.get();
-        return BuiltInRegistries.BLOCK.get(new ResourceLocation(configValue));
-    }
+import java.util.function.Supplier;
 
-    public SiennaCrop(Properties properties) {
+public class BeetifulCropBlock extends BeetrootBlock implements IPlantable {
+    private final Supplier<String> plantableOnConfig;
+
+    public BeetifulCropBlock(Properties properties, Supplier<String> plantableOnConfig) {
         super(properties);
+        this.plantableOnConfig = plantableOnConfig;
     }
 
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
-        return state.is(getPlantableOn());
+        String configValue = plantableOnConfig.get();
+        Block block = BuiltInRegistries.BLOCK.get(new ResourceLocation(configValue));
+        return state.is(block);
     }
 
     @Override
