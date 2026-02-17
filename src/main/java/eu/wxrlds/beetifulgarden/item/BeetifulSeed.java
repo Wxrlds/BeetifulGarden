453c96fd7065de538cf4bb82bae657ceeb0824cd
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-17 21:02:29 +0100

    Only allow placing the seed when clicking the top of the block

--- src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulSeed.java
+++ src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulSeed.java
@@ -4,9 +4,11 @@ import eu.wxrlds.beetifulgarden.BeetType;
 import eu.wxrlds.beetifulgarden.block.ModBlocks;
 import eu.wxrlds.beetifulgarden.util.PlantableOnParser;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -20,11 +22,14 @@ public class BeetifulSeed extends Item {
     public ActionResultType useOn(ItemUseContext context) {
         World world = context.getLevel();
         BlockPos clickPos = context.getClickedPos();
+        if (context.getClickedFace() != Direction.UP) {
+            return ActionResultType.PASS;
+        }
         BlockPos spawnPos = clickPos.above();
         Block blockBelow = world.getBlockState(clickPos).getBlock();
 
         // Ensure the space above is air/replaceable
-        if (!world.getBlockState(spawnPos).getMaterial().isReplaceable()) {
+        if (!world.getBlockState(spawnPos).canBeReplaced(new BlockItemUseContext(context))) {
             return ActionResultType.PASS;
         }
 
