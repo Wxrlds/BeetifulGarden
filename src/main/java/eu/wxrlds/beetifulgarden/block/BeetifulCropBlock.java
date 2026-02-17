bfab8cede088669f4d1be09231e6fd9801bfa8c2
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-17 17:18:04 +0100

    Improve reading the plantable on config, as well as the logic to enforce this. Allow placing seeds on multiple blocks

--- src/main/java/eu/wxrlds/beetifulgarden/block/BeetifulCropBlock.java
+++ src/main/java/eu/wxrlds/beetifulgarden/block/BeetifulCropBlock.java
@@ -1,13 +1,11 @@
 package eu.wxrlds.beetifulgarden.block;
 
 import eu.wxrlds.beetifulgarden.item.ModItems;
+import eu.wxrlds.beetifulgarden.util.PlantableOnParser;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.BeetrootBlock;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.common.IPlantable;
 
@@ -23,9 +21,7 @@ public class BeetifulCropBlock extends BeetrootBlock implements IPlantable {
 
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
-        String configValue = plantableOnConfig.get();
-        Block block = BuiltInRegistries.BLOCK.get(new ResourceLocation(configValue));
-        return state.is(block);
+        return PlantableOnParser.isAllowed(state.getBlock(), plantableOnConfig.get());
     }
 
     @Override
