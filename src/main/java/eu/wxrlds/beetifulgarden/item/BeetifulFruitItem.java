7b6ec3d018e8f304dab3789311b3e8af0dfc7eb0
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-18 16:08:47 +0100

    Apply effects using finishUsingItem

--- src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulFruitItem.java
+++ src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulFruitItem.java
@@ -1,14 +1,16 @@
 package eu.wxrlds.beetifulgarden.item;
 
 import eu.wxrlds.beetifulgarden.BeetType;
-import eu.wxrlds.beetifulgarden.util.EffectsParser;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
@@ -18,10 +20,6 @@ public class BeetifulFruitItem extends Item {
     public BeetifulFruitItem(Properties properties, BeetType type) {
         super(properties.food(new FoodProperties.Builder().alwaysEdible().build()));
         this.type = type;
-    }
-
-    public String getEffectString() {
-        return type.getEffects();
     }
 
     public int getNutrition() {
@@ -35,5 +33,18 @@ public class BeetifulFruitItem extends Item {
     @Override
     public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
         PotionContents.addPotionTooltip(type.getParsedEffects(), tooltipComponents::add, 1.0F, context.tickRate());
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entity) {
+        if (!world.isClientSide && entity instanceof Player player) {
+
+            for (MobEffectInstance effect : type.getParsedEffects()) {
+                player.addEffect(new MobEffectInstance(effect));
+            }
+
+            player.getFoodData().eat(getNutrition(), getSaturation());
+        }
+        return super.finishUsingItem(stack, world, entity);
     }
 }
