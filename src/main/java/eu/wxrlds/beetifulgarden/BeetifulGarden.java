6105ac2b10ff7bd5050290e9b82ec610784846b7
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-18 20:10:06 +0100

    Version bump to 1.20.1-1.2.0

--- src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
+++ src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
@@ -12,20 +12,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BeetifulGarden.MOD_ID)
 public class BeetifulGarden {
     public static final String MOD_ID = "beetifulgarden";
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public BeetifulGarden(FMLJavaModLoadingContext context) {
         IEventBus eventBus = context.getModEventBus();
 
-        // Config file
         context.registerConfig(ModConfig.Type.COMMON, BeetifulGardenCommonConfigs.SPEC, "beetifulgarden-common.toml");
 
-        // Register items and blocks
         ModItems.ITEMS.register(eventBus);
         ModItems.register();
         ModBlocks.BLOCKS.register(eventBus);
@@ -34,12 +30,10 @@ public class BeetifulGarden {
 
         eventBus.addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
         LOGGER.info("HELLO FROM THE BEETIFUL WORLD");
     }
 }
