a37ecadc623a2a85a0f3316184fd82b694a20042
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2026-02-18 20:07:22 +0100

    Version bump to 1.18.2-1.2.0

--- src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
+++ src/main/java/eu/wxrlds/beetifulgarden/BeetifulGarden.java
@@ -21,43 +21,33 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BeetifulGarden.MOD_ID)
 public class BeetifulGarden {
     public static final String MOD_ID = "beetifulgarden";
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public BeetifulGarden() {
-        // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Config file
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BeetifulGardenCommonConfigs.SPEC, "beetifulgarden-common.toml");
 
-        // Register items and blocks
         ModItems.ITEMS.register(eventBus);
         ModItems.register();
         ModBlocks.BLOCKS.register(eventBus);
         ModBlocks.register();
 
         eventBus.addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         eventBus.addListener(this::enqueueIMC);
-        // Register the doClientStuff method for modloading
         eventBus.addListener(this::doClientStuff);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
         LOGGER.info("HELLO FROM THE BEETIFUL WORLD");
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // Configure rendering of the crops
         ModBlocks.CROP_BLOCKS.values().forEach(registryObject -> {
             ItemBlockRenderTypes.setRenderLayer(registryObject.get(), RenderType.cutout());
         });
